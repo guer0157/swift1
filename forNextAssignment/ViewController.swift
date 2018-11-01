29d865a3d5418a6827a098061351016e295ab137
guer0157 <32390693+guer0157@users.noreply.github.com>
2018-11-01 15:47:57 -0400

    Initial Commit

---
+++ forNextAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  forNextAssignment
+//
+//  Created by Cesar Guerrero on 2018-11-01.
+//  Copyright © 2018 guer0157.AlgonquinCollege. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
